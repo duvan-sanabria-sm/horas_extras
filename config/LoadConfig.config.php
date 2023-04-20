6345767d826c977ca9f8b1c2ef9d3de838b4facd
William Ricardo Enciso Bautista <william.enciso@servimeterssa.com>
2023-04-20 10:25:11 -0500

    Actualizacion ambiente de prod

--- config/LoadConfig.config.php
+++ config/LoadConfig.config.php
@@ -3,12 +3,13 @@
 class LoadConfig
 {
     private static $path;
-    public static function getConfig(){
-        self::$path = realpath($_SERVER["DOCUMENT_ROOT"] .'/HorasExtra/config/config.json');
-        if (file_exists(self::$path)){
+    public static function getConfig()
+    {
+        self::$path = realpath($_SERVER["DOCUMENT_ROOT"] . '/config/config.json');
+        if (file_exists(self::$path)) {
             $json_data = file_get_contents(self::$path);
             return json_decode($json_data);
-        }else{
+        } else {
             return 'No hay archivo';
         }
     }
