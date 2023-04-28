5520da21ec870af6cb0114272fb3a6964b102f87
William Ricardo Enciso Bautista <william.enciso@servimeterssa.com>
2023-04-28 11:54:21 -0500

    Issue - Envio de doble correo | Solucionado

--- config/SendEmail.config.php
+++ config/SendEmail.config.php
@@ -2,12 +2,14 @@
 
 use PHPMailer\PHPMailer\PHPMailer;
 
-class Email{
+class Email
+{
 
     private $correo;
     private $config;
 
-    function __construct(){
+    function __construct()
+    {
         include_once('../config/PhpMailer/Exception.php');
         include_once('../config/PhpMailer/PHPMailer.php');
         include_once('../config/PhpMailer/SMTP.php');
@@ -17,39 +19,37 @@ class Email{
     }
 
 
-    public function sendEmail($to, $cc, $subject, $body){
+    public function sendEmail($to, $cc, $subject, $body)
+    {
 
-        try{
-            $this->correo->SMTPDebug=0;
+        try {
+            $this->correo->SMTPDebug = 0;
             $this->correo->isSMTP();
-            $this->correo->Host= $this->config->HOST_EMAIL;
-            $this->correo->SMTPAuth=true;
-            $this->correo->Username=$this->config->USERNAME_EMAIL;
-            $this->correo->Password= $this->config->PASS_EMAIL;
-            $this->correo->SMTPSecure="tls";
-            $this->correo->Port=$this->config->PORT_EMAIL;
-        
-            $this->correo->setFrom($this->config->FROM_EMAIL,"Solicitud de Horas Extra");
+            $this->correo->Host = $this->config->HOST_EMAIL;
+            $this->correo->SMTPAuth = true;
+            $this->correo->Username = $this->config->USERNAME_EMAIL;
+            $this->correo->Password = $this->config->PASS_EMAIL;
+            $this->correo->SMTPSecure = "tls";
+            $this->correo->Port = $this->config->PORT_EMAIL;
+
+            $this->correo->setFrom($this->config->FROM_EMAIL, "Solicitud de Horas Extra");
             $this->correo->addAddress($to);
             $this->correo->addAddress($this->config->FROM_EMAIL);
             $this->correo->addCC($cc);
             $this->correo->isHTML(true);
-            $this->correo->Subject=$subject;
-            $this->correo->Body=$body;
-            $this->correo->CharSet="UTF-8";
-            $this->correo->send();
+            $this->correo->Subject = $subject;
+            $this->correo->Body = $body;
+            $this->correo->CharSet = "UTF-8";
 
-            if(!$this->correo->send()) {
+            if (!$this->correo->send()) {
                 echo 'Message could not be sent.';
                 echo 'Mailer Error: ' . $this->correo->ErrorInfo;
             } else {
                 echo 'Message has been sent';
             }
             return true;
-        }
-        catch (Exception $e){
+        } catch (Exception $e) {
             return $this->correo->ErrorInfo;
         }
-
     }
 }
