1653e8d423e354b0f9300ba191b943de2661e7bd
William Ricardo Enciso Bautista <william.enciso@servimeterssa.com>
2023-05-10 17:04:02 -0500

    Route server

--- config/LoadConfig.config.php
+++ config/LoadConfig.config.php
@@ -5,7 +5,7 @@ class LoadConfig
     private static $path;
     public static function getConfig()
     {
-        self::$path = realpath($_SERVER["DOCUMENT_ROOT"] . '/config/config.json');
+        self::$path = realpath($_SERVER["DOCUMENT_ROOT"] . '/' . explode("/", $_SERVER['REQUEST_URI'])[1] . '/config/config.json');
         if (file_exists(self::$path)) {
             $json_data = file_get_contents(self::$path);
             return json_decode($json_data);
